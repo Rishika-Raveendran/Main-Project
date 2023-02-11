31783c1398447285e95ecf91cc57fc8262f56a4a
Aishwarya-S-Prabhu <ayshwaria.achu@gmail.com>
2023-02-11 20:09:54 +0530

    first commit

---
+++ dashboard/urls.py
@@ -0,0 +1,10 @@
+from tkinter import N
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='dashboard-index'),
+    path('staff/', views.staff, name='dashboard-staff'),
+    path('users/', views.users, name='dashboard-users'),
+    path('sales/', views.sales, name='dashboard-sales')
+]
